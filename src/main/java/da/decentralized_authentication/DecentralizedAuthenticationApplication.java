cfa0a3532372515734ec3fcc510ff1ac0d4f9125
Vasilxb <blazhevskivvasil@gmail.com>
2026-08-14 20:43:29 +0200

    Initial commit

---
+++ src/main/java/da/decentralized_authentication/DecentralizedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package da.decentralized_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentralizedAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecentralizedAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/da/decentralized_authentication/DecentralizedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package da.decentralized_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentralizedAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
